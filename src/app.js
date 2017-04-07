1e1a48b62dedfb764e27bcf7f789d7b40a289331
shizkun <pivotanimator95@gmail.com>
2017-04-08 07:55:45 +0800

    reorganize js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "./scss/master.scss";
 import * as $ from "jquery";
 import * as ko from "knockout";
 import * as customBinding from "./js/custom-bindings.js"
-import { ViewModel } from "./models/main-viewmodel.js";
+import { ViewModel } from "./js/models/main-viewmodel.js";
 
 $(document).ready(function() {
 	let vm = new ViewModel();
