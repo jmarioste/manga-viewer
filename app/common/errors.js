3f765d686f72d6f728c8a67c214176c052cbb627
shizkun <knightjaz83@gmail.com>
2017-05-18 07:57:32 +0800

    Add not supported error

--- app/common/errors.js
+++ app/common/errors.js
@@ -2,5 +2,6 @@ module.exports = {
     TIMEOUT_OPEN_RARFILE_ERR: "Timeout in opening the rar file",
     NO_IMAGE_FILE: "No images in the file",
     FileDoesNotExist: "File does not exist",
-    CannotOpenFile: "Cannot open file"
+    CannotOpenFile: "Cannot open file",
+    NotSupported: "Not supported file type."
 }
