aadc041da07d65a4d243a4e16e622409140323cd
shizkun <knightjaz83@gmail.com>
2017-05-23 15:31:21 +0800

    change e2e/data to .data

--- app/main.js
+++ app/main.js
@@ -15,6 +15,7 @@ if (isDev && process.env.NODE_ENV === "development") {
 
 if (process.env.SPECTRON) {
     logger.debug('Changing paths for Spectron');
+    app.setPath('userData', path.resolve(app.getAppPath(), ".data"))
     mock(dialog)
 }
 // Keep a global reference of the window object, if you don't, the window will
