df2bbf9beb50f5627c434b01c2b8c9158ca72941
shizkun <knightjaz83@gmail.com>
2017-05-24 13:29:49 +0800

    TG-132 #in-progress
    Guarantee that current page always starts in manga-list

--- e2e/manga-list.spec.js
+++ e2e/manga-list.spec.js
@@ -6,12 +6,17 @@ const expect = chai.expect;
 
 describe('Manga List', function () {
     this.timeout(30000)
-    before(function () {
-        // console.log(browser);
-        return setup.removeAppData();
-    });
+    // before(function () {
+    //     // console.log(browser);
+    //     return setup.removeAppData();
+    // });
 
-    beforeEach(setup.initApp);
+    beforeEach(function () {
+        return setup.removeAppData()
+            .then(function () {
+                return setup.initApp();
+            })
+    });
 
     afterEach(setup.stopApp)
 
@@ -34,6 +39,7 @@ describe('Manga List', function () {
         it('it should be added under bookmarked folders in sidebar', function () {
             let bookmarkedFolders = ".sidebar-favorites-items .collection-item";
             return setup.app.client
+                .selectDirectorySampleManga()
                 .bookmarkFolder()
                 .pause(500)
                 .getElementCount(bookmarkedFolders).should.eventually.equal(1);
@@ -44,6 +50,9 @@ describe('Manga List', function () {
         it('it should be removed from bookmarked folders', function () {
             let bookmarkedFolders = ".sidebar-favorites-items .collection-item";
             return setup.app.client
+                .selectDirectorySampleManga()
+                .bookmarkFolder()
+                .pause(500)
                 .bookmarkFolder()
                 .pause(500)
                 .getElementCount(bookmarkedFolders).should.eventually.equal(0);
@@ -63,6 +72,7 @@ describe('Manga List', function () {
 
                 it(`should return ${test.expected} manga(s)`, () => {
                     return setup.app.client
+                        .selectDirectorySampleManga()
                         .waitForFinishLoading()
                         .searchManga(test.searchValue)
                         .waitForFinishLoading()
@@ -78,6 +88,7 @@ describe('Manga List', function () {
 
         it('Should display all the mangas recursively', () => {
             return setup.app.client
+                .selectDirectorySampleManga()
                 .waitForFinishLoading()
                 .click(".include-subfolders > label.right")
                 .pause(500)
@@ -91,6 +102,7 @@ describe('Manga List', function () {
 
         it('it should transfer to view manga page', () => {
             return setup.app.client
+                .selectDirectorySampleManga()
                 .waitForFinishLoading()
                 .element("#mangalist .manga")
                 .click()
@@ -104,20 +116,39 @@ describe('Manga List', function () {
     describe('Adding/Removing manga to/from favorites', () => {
         let expectedTexts = ["Added to favorites!", "Removed from favorites!"];
 
-        expectedTexts.forEach(function (expectedText) {
-            it(`it should show a toast with "${expectedText}" as feedback`, () => {
+
+        describe('When manga is not yet a favorite', () => {
+            it(`it should show a toast with "Added to favorites!" as feedback`, () => {
                 let toastContainer = "#toast-container div";
                 return setup.app.client
+                    .selectDirectorySampleManga()
                     .waitForFinishLoading()
                     .element("#mangalist .manga .toggle-as-favorite")
                     .click()
                     .isExisting(toastContainer).should.eventually.be.true
-                    .getText(toastContainer).should.eventually.equal(expectedText)
+                    .getText(toastContainer).should.eventually.equal("Added to favorites!")
                     .waitForExist(toastContainer, 10000, true)
                     .isExisting(toastContainer).should.eventually.be.false
             })
-        })
+        });
 
+        describe('When manga is already a favorite', () => {
+            it(`it should show a toast with "Remove from favorites!" as feedback`, () => {
+                let toastContainer = "#toast-container div";
+                return setup.app.client
+                    .selectDirectorySampleManga()
+                    .waitForFinishLoading()
+                    .element("#mangalist .manga .toggle-as-favorite")
+                    .click()
+                    .pause(1000)
+                    .element("#mangalist .manga .toggle-as-favorite")
+                    .click()
+                    .isExisting(toastContainer).should.eventually.be.true
+                    .getText(toastContainer).should.eventually.equal("Removed from favorites!")
+                    .waitForExist(toastContainer, 10000, true)
+                    .isExisting(toastContainer).should.eventually.be.false
+            })
+        });
     });
 
 })
