4c3f236bae75fa89f39340a0509c4419b78ae52e
jmarioste <pivotanimator95@gmail.com>
2018-04-28 22:14:00 +0800

    Add magnifiy image

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -20,6 +20,7 @@ let vm = {
 $(document).ready(function () {
     $('.loading').delay(500).fadeOut(500);
     $('.carousel').carousel()
+    $('.do-magnifiy').magnificPopup({ type: 'image' });
 
     fetch('https://github.com/jmarioste/manga-viewer/releases')
         .then(function (data) {
