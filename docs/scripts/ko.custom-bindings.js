efc41fc7219c346f6619df52bb3572573da0ebd5
jmarioste <pivotanimator95@gmail.com>
2018-05-04 01:56:50 +0800

    Update docs

--- docs/scripts/ko.custom-bindings.js
+++ docs/scripts/ko.custom-bindings.js
@@ -1,13 +1,20 @@
 
-ko.bindingHandlers.highlightDownload = {
+ko.bindingHandlers.hideNotPlatform = {
     init: function (element, valueAccessor) {
         const value = ko.unwrap(valueAccessor());
         const platform = navigator.platform.toLowerCase();
         const isPlatform = platform.indexOf(value) >= 0;
         console.log(isPlatform, value);
-        if (isPlatform) {
+        if (!isPlatform) {
             console.log("applying active");
-            $(element).addClass("active-btn")
+            $(element).hide();
         }
+    }
+}
+
+ko.bindingHandlers.href = {
+    init: function (element, valueAccessor) {
+        const value = ko.unwrap(valueAccessor());
+        $(element).attr('href', value);
     }
 }
--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,23 +1,24 @@
 let vm = {
-    downloadButtons: [
-        {
-            label: "Download for mac",
-            platform: "mac",
-        },
-        {
-            label: "Download for windows",
-            platform: "win",
-        },
-        {
-            label: "Download for linux",
-            platform: "linux",
-        }
-    ],
+    version: "1.2.2",
+    downloads: {
+        AppImage: "https://github.com/jmarioste/manga-viewer/releases/download/v1.2.2/baiji-manga-viewer-1.2.2-x86_64.AppImage",
+        dmg: "https://github.com/jmarioste/manga-viewer/releases/download/v1.2.2/baiji-manga-viewer-1.2.2.dmg",
+        exe: "https://github.com/jmarioste/manga-viewer/releases/download/v1.2.2/baiji-manga-viewer-setup-1.2.2.exe",
+        deb: "https://github.com/jmarioste/manga-viewer/releases/download/v1.2.2/baiji-manga-viewer_1.2.2_amd64.deb",
+        zip: "https://github.com/jmarioste/manga-viewer/releases/download/v1.2.2/baiji-manga-viewer-1.2.2-mac.zip"
+
+    },
+
     getUrl(assets, regex) {
         var url = assets.map(asset => asset.browser_download_url)
             .filter((url) => regex.test(url));
         console.log(url);
         return url[0];
+    },
+    getDownloadCount(assets) {
+        var stuff = assets.map(asset => asset.download_count)
+        console.log(stuff);
+        return stuff;
     }
 };
 $(document).ready(function () {
@@ -25,20 +26,24 @@ $(document).ready(function () {
     $('.carousel').carousel()
     $('.do-magnifiy').magnificPopup({ type: 'image' });
 
-    fetch('https://api.github.com/repos/jmarioste/manga-viewer/releases/latest')
-        .then(function (response) {
-            return response.json();
-        }).then(function(data){
-            console.log(data);
-            vm.downloadButtons[0].href = vm.getUrl(data.assets, /.dmg$/g);
-            vm.downloadButtons[1].href = vm.getUrl(data.assets, /.exe$/g);
-            vm.downloadButtons[2].href = vm.getUrl(data.assets, /.deb$/g);
-            
-            ko.applyBindings(vm, $("body")[0]);
-        })
-           
-        // .catch(function () {
-        //     console.log("nothing happend");
-        // })
-    
+    // fetch('https://api.github.com/repos/jmarioste/manga-viewer/releases/latest')
+    //     .then(function (response) {
+    //         return response.json();
+    //     }).then(function (data) {
+    //         console.log(data);
+    //         vm.downloadButtons[0].href = vm.getUrl(data.assets, /.dmg$/g);
+    //         vm.downloadButtons[1].href = vm.getUrl(data.assets, /.exe$/g);
+    //         vm.downloadButtons[2].href = vm.getUrl(data.assets, /.deb$/g);
+    //         console.log(vm.getDownloadCount(data.assets));
+    //         ko.applyBindings(vm, $("body")[0]);
+
+    //     })
+
+    // vm.downloadButtons[0].href = vm.getUrl(data.assets, /.dmg$/g);
+    // vm.downloadButtons[1].href = vm.getUrl(data.assets, /.exe$/g);
+    // vm.downloadButtons[2].href = vm.getUrl(data.assets, /.deb$/g);
+    // .catch(function () {
+    //     console.log("nothing happend");
+    // })
+    ko.applyBindings(vm, $("body")[0]);
 })
