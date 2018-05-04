f3f588dfde487d40b852aed2cfeedf9245a069b4
jmarioste <jassermark.arioste@gmail.com>
2018-05-04 16:29:35 +0800

    Stuff

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,8 +1,8 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Method GET -Uri https://api.github.com/repos/jmarioste/manga-viewer/releases/latest?auth_token=$env:GH_TOKEN -ContentType "json" -OutFile ./docs/scripts/metadata.json --quiet --no-verbose >$null 2>&1
+Invoke-RestMethod -Method GET -Uri https://api.github.com/repos/jmarioste/manga-viewer/releases/latest?auth_token=$env:GH_TOKEN -ContentType "json" -OutFile ./docs/scripts/metadata.json >$null 2>&1
 
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
+git config --global user.email "jassermark.arioste@gmail.com"
+git config --global user.name "Jasser Mark Arioste"
 
 rm -r docs-generated/
 npm run docs
@@ -10,5 +10,5 @@ npm run docs
 cd docs-generated
 git init
 git add .
-git commit -m "Deploy to Github Pages"
+git commit -m "Automated push to Github Pages"
 git push --force --quiet "https://$env:GH_TOKEN@github.com/jmarioste/manga-viewer.git" master:gh-pages --no-verbose >$null 2>&1
